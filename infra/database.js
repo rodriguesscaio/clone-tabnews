dbf85aa8db40b350ff0fdbd5e2d633a2609d0d75
rodriguesscaio <caio.rodrigues.barbosa@alumni.usp.br>
2024-05-22 21:57:04 -0300

    Refactor: make file `database.js` more robust to handler error

--- infra/database.js
+++ infra/database.js
@@ -9,13 +9,24 @@ async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD,
   });
 
-  await client.connect();
+  console.log(
+    "Credentials database:" +
+      {
+        user: process.env.POSTGRES_USER,
+        database: process.env.POSTGRES_DB,
+        port: process.env.POSTGRES_PORT,
+        host: process.env.POSTGRES_HOST,
+        password: process.env.POSTGRES_PASSWORD,
+      },
+  );
 
   try {
+    await client.connect();
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
     console.error(error);
+    throw error;
   } finally {
     client.end();
   }
