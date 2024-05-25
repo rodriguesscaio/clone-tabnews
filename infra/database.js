ce85ce1bc1b99a0dd900e7750f579eae64137089
rodriguesscaio <caio.rodrigues.barbosa@alumni.usp.br>
2024-05-25 19:55:32 -0300

    make `database.js` more robust to handler with different environment

--- infra/database.js
+++ infra/database.js
@@ -7,6 +7,7 @@ async function query(queryObject) {
     port: process.env.POSTGRES_PORT,
     host: process.env.POSTGRES_HOST,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV == "development" ? false : true,
   });
 
   console.log("Credentials database:", {
